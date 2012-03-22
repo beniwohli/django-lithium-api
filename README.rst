2bcc6fad5226ce74f2a43f8c3cce6589125937ed
Benjamin Wohlwend <piquadrat@gmail.com>
2012-03-22 17:07:07 +0100

    setup.py stuff

---
+++ README.rst
@@ -0,0 +1,9 @@
+django lithium API
+==================
+
+This is a very limited implementation of the `Lithium REST API`_.
+
+Read the documentation to find out how to use it. Oh, wait, there are no docs.
+That probably means you shouldn't be using this just yet...
+
+.. _Lithium REST API: http://lithosphere.lithium.com/t5/Documentation-TKB/Admin-Guide-REST-API-JavaDocs/ta-p/5966
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+from django_lithium_api import __version__ as version
+
+install_requires = [
+    'setuptools',
+]
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'django-lithium-api',
+    version = version,
+    description = "A (very) limited implementation of the Lithium REST API for Django",
+    long_description = read('README.rst'),
+    author = 'Benjamin Wohlwend',
+    author_email = 'piquadrat@gmail.com',
+    url = 'https://github.com/piquadrat/django-lithium-api',
+    packages = find_packages(),
+    zip_safe=False,
+    include_package_data = True,
+    install_requires=[
+        'Django>=1.3',
+        ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ]
+)
