b8963c2a3ad404df28a74fcb88a08ff5f0ad09bb
Benjamin Wohlwend <piquadrat@gmail.com>
2013-10-21 16:56:37 +0200

    handle pagination in lithium_import

--- django_lithium_api/management/commands/lithium_import.py
+++ django_lithium_api/management/commands/lithium_import.py
@@ -19,11 +19,25 @@ Style:\t%s
             """ % (board.title.encode('utf-8'), board.id, board.href, board.interaction_style)
 
     def handle_messages(self, *args, **options):
+        page_size = 100
         board_id = args[1]
         if '*' in board_id or '?' in board_id:
             boards = [board.id for board in self.api('boards_nested') if fnmatch(board.id, board_id)]
         else:
             boards = [board_id]
         for board in boards:
-            for message in self.api('messages_linear', full_detail=True,  board_id=board):
-                api_signals.message_create.send(self, message=message, token=None, raw_xml=etree.tostring(message._tree))
+            page = 1
+            messages = self.get_messages(board, page, page_size)
+            while messages:
+                for message in messages:
+                    api_signals.message_create.send(self, message=message, token=None, raw_xml=etree.tostring(message._tree))
+                if len(messages) <= page_size:
+                    # turns out lithium just keeps returning the results of the very last page, no matter what page
+                    # number we use
+                    break
+                page += 1
+                messages = self.get_messages(board, page, page_size)
+
+
+    def get_messages(self, board, page, page_size):
+        return self.api('messages_linear', full_detail=True,  board_id=board, data={'page_size': page_size, 'page': page})
